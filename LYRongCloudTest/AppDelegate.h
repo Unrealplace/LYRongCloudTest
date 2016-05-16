03e2fd4ec34032983e14affa60bb8fa0f2ddde9b
Unrealplace <liyang84681111819@qq.com>
2016-05-16 11:35:41 +0800

    Initial Commit

---
+++ LYRongCloudTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYRongCloudTest
+//
+//  Created by LiYang on 16/5/16.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYRongCloudTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYRongCloudTest
+//
+//  Created by LiYang on 16/5/16.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
